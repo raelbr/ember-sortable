48906e810898b6dba36e1940bf07176174106eef
Rael B. Riolino <rael.riolino@venturus.org.br>
2016-03-03 07:41:56 -0300

    fix for the infinite drag bug

--- addon/mixins/sortable-item.js
+++ addon/mixins/sortable-item.js
@@ -291,7 +291,7 @@ export default Mixin.create({
     }
 
     $(window).one('mousemove touchmove', startDragListener);
-    $(window).one('mouseup touchend', cancelStartDragListener);
+    $(window).one('click mouseup touchend', cancelStartDragListener);
   },
 
   /**
@@ -306,14 +306,14 @@ export default Mixin.create({
     let drop = () => {
       $(window)
         .off('mousemove touchmove', drag)
-        .off('mouseup touchend', drop);
+        .off('click mouseup touchend', drop);
 
       this._drop();
     };
 
     $(window)
       .on('mousemove touchmove', drag)
-      .on('mouseup touchend', drop);
+      .on('click mouseup touchend', drop);
 
     this._tellGroup('prepare');
     this.set('isDragging', true);
